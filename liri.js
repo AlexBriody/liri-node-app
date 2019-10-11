481cc4bafc27e768f1d3929e37bb2aa300028f27
Alex Briody <alexbriody@gmail.com>
2019-10-11 12:26:25 -0400

    OMDB function

--- liri.js
+++ liri.js
@@ -36,7 +36,8 @@ switch (userChoice) {
 
 function concertThis(userThing) {
     axios.get("https://rest.bandsintown.com/artists/" + userThing + "/events?app_id=codingbootcamp").then(function (response) {
-        console.log("Artist: " + userThing)
+        //console.log(response);
+        //console.log(response.data);
         console.log("Name of the venue: ", response.data[0].venue.name);
         console.log("Venue location: " + response.data[0].venue.city);
         var eventDate = moment(response.data[0].datetime).format("MM/DD/YYYY");
@@ -90,4 +91,31 @@ function defaultSpotify (){
         .catch(function (err) {
             console.log(err);
         });
+}
+
+function movieThis(userThing) {
+    if (!userThing) {
+        userThing = "Mr.Nobody";
+    }
+    axios.get("https://www.omdbapi.com/?t=" + userThing + "&y=&plot=short&apikey=trilogy").then(function (response) {
+        //console.log(response);
+        //console.log(response.data)
+        console.log("Title of the movie: " + response.data.Title);
+        console.log("Year the movie came out: " + response.data.Year);
+        console.log("IMDB Rating of the movie: " + response.data.Ratings[0].Value);
+        console.log("Rotten Tomatoes Rating of the movie: " + response.data.Ratings[1].Value);
+        console.log("Country where the movie was produced: " + response.data.Country);
+        console.log("Language of the movie: " + response.data.Language);
+        console.log("Plot of the movie: " + response.data.Plot);
+        console.log("Actors in the movie: " + response.data.Actors);
+    })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+
+
+
+
+
 }
